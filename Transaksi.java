5525a41b0d2d959a123059b92c83021ff56a7ad0
arissandya <32530685+arissandya@users.noreply.github.com>
2017-11-25 11:09:23 +0700

    java

---
+++ Transaksi.java
@@ -0,0 +1,30 @@
+class Transaksi{
+	public String NamaTransaksi;
+	public String JenisTranskasi;
+	public String NomorTransaksi;
+	public Int NomorKwitansi;
+	
+
+	public Cetak (){
+	scanner input = new scanner(System.in);
+		System.out.print("Masukan NomorTransaksi");
+		NamaTransaksi = input.nextline();
+		System.out.print("Masukan JenisTranskasi:");
+	    JenisTransaksi = input.nextline();
+	    System.out.print("Masukan NomorTransaksi:");
+	    NomorTransaksi = input.nextline();
+	    System.out.print("Masukan NomorKwitansi:");
+	    NomorKwitansi = input.nextline();
+	 }
+	public Void menjual(){
+	scanner input = new scanner(System.in);
+		System.out.print("Masukan NomorTransaksi");
+		NamaTransaksi = input.nextline();
+		System.out.print("Masukan JenisTranskasi:");
+	    JenisTransaksi = input.nextline();
+	    System.out.print("Masukan NomorTransaksi:");
+	    NomorTransaksi = input.nextline();
+	    System.out.print("Masukan NomorKwitansi:");
+	    NomorKwitansi = input.nextline();
+	 }
+
