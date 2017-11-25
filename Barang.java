361280f6bd11cd3899f5e84172644551ce251621
arissandya <32530685+arissandya@users.noreply.github.com>
2017-11-25 11:09:05 +0700

    java

---
+++ Barang.java
@@ -0,0 +1,27 @@
+class Barang{
+	public String Namabarang;
+	public Int HargaBarang;
+	public String JenisBarang;
+	
+
+	public Void inputkan(){
+	scanner input = new scanner(System.in);
+		System.out.print("Masukan NamaBarang");
+		NamaBarang = input.nextline();
+		System.out.print("Masukan HargaBarang:");
+	    HargaBarang = input.nextline();
+	    System.out.print("Masukan JenisBarang:");
+	    JenisBarang = input.nextline();
+	 }
+	public Void hapus(){
+	scanner input = new scanner(System.in);
+		System.out.print("Masukan NamaBarang");
+		NamaBarang = input.nextline();
+		System.out.print("Masukan HargaBarang:");
+	    HargaBarang = input.nextline();
+	    System.out.print("Masukan JenisBarang:");
+	    JenisBarang = input.nextline();
+	 }
+
+
+}
