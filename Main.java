12662e6d0c07fff6c706183d1b60ecd07ab52682
arissandya <32530685+arissandya@users.noreply.github.com>
2017-11-25 11:08:36 +0700

    java

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Main {
+	public static void Barang (String()args) {
+		Barang brg = new Barang();
+		Barang.Inputkan();
+		Scanner input = new Scanner(System.in);
+		NamaBarang = input.nextLine();
+		HargaBarang = input.nextLine();
+		JenisBarang = input.nextLine();	
+	}
+	public static void  Transaksi (String()args) {
+		Transaksi lap = new Transaksi();
+		Transaksi.Cetak();
+		Scanner input = new Scanner(System.in);
+		NamaTransaksi = input.nextLine();
+		JenisTransaksi = input.nextLine();
+		NomorTransaksi = input.nextLine();
+		NomorKwitansi = inpuxt.nextLine();
+	}
+	
+	public static void Admin (String()args) {
+		NamaAdmin adm = new NamaAdmin();
+		IdAdminid = new IdAdmin();
+		Scanner input = new Scanner(system.in);
+		NamaAdmin = input.nextLine();
+		IdAdmin = input.nextLine();
+		
+	}
+}
